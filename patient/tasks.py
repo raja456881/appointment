0a38a1412c06423b5c74045f66802e3a597d8511
raj <61022554+raja456881@users.noreply.github.com>
2023-07-25 21:19:54 +0530

    Update tasks.py

--- patient/tasks.py
+++ patient/tasks.py
@@ -2,13 +2,13 @@ from celery import shared_task
 from twilio.rest import Client
 import time
 import json
-# from decouple import config
+from decouple import config
 
 @shared_task(bind=True)
 def send_reminder_patient(self, phone):
-    Accound_sid="AC28d8d98debaa39135dc71d67a03bd1fb"
-    Auth_token="bdba2978ba41a8800885fbf5423b12a0"
-    phone_no =+14327772801
+    Accound_sid=config("Accound_sid")
+    Auth_token=config("Auth_token")
+    phone_no =config("phone_no")
     client = Client(Accound_sid, Auth_token)
     client.messages.create(from_=phone_no,
                       to=phone,
